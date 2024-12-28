5e0093d3ee273e7b6e2205bc5095c191fc351559
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-12-28 23:30:44 +0530

    hashed functionality for security

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -10,11 +10,14 @@ const register = async (req, res) => {
       if (ExistUser) {
          res.status(500).json({ success: true, message: 'User already exist' })
       }
+       // Hash the password
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(password, salt);
    //   create user
       const user = await User.create({
          fullName: fullName,
          email: email,
-         password: password
+         password: hashedPassword
       })
    // save user in database
       user.save();
